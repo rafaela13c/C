169434994fa7775c3d7a9325a2840d76eb25ded9
Rafaela Cristina Alcântara da Silva <124393270+rafaela13c@users.noreply.github.com>
2023-09-04 12:35:51 -0300

    Add files via upload

---
+++ Estrutura de repetição/3 - Intervalos de números (do while).c
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int quant1 = 0, quant2 = 0, quant3 = 0, quant4 = 0;
+    
+    do {
+    printf("Digite um número (ou um número negativo para sair): ");
+    scanf("%d", &num);
+    
+    if (num >= 0 && num <= 25) {
+        quant1++;
+        
+    } else if (num > 25 && num <= 50) {
+        quant2++;
+      
+    } else if (num > 50 && num <= 75) {
+        quant3++;
+    
+    } else if (num > 75 && num <= 100) {
+        quant4++;
+    }
+        } while (num >= 0);
+         printf("%d números digitados estão dentro do intervalo [0, 25]\n", quant1);
+         printf("%d números digitados estão dentro do intervalo [26, 50]\n", quant2);
+         printf("%d números digitados estão dentro do intervalo [51, 75]\n", quant3);
+         printf("%d números digitados estão dentro do intervalo [76, 100]\n", quant4);
+        
+
+
+    return 0;
+}
