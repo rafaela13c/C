d881902bae5136e091c1a6d4f0745d11a23bb7f2
Rafaela Cristina Alcântara da Silva <124393270+rafaela13c@users.noreply.github.com>
2023-09-06 10:09:55 -0300

    Update 1 - Média e código do aluno (while).c

--- Estrutura de repetição/1 - Média e código do aluno (while).c
+++ Estrutura de repetição/1 - Média e código do aluno (while).c
@@ -1,17 +1,22 @@
-
 #include <stdio.h>
 
 int main()
 {
     int n1, n2, n3, cod;
  
+    while (1) {
+        
     printf("Digite o codigo do aluno: ");
     scanf("%d", &cod);
     
-    while (1) {
-           
-    printf("Digite as três notas do aluno (ou 0 para sair): ");
+            if (cod == 0) {
+            break;
+        }
+        
+    printf("Digite as três notas do aluno: ");
     scanf("%d %d %d", &n1, &n2, &n3);
+    
+   
    
         int med = (n1 + n2 + n3) / 3;
         printf("O código do aluno é: %d e a sua média é igual a: %d\n", cod, med);
@@ -21,3 +26,4 @@ int main()
 
     return 0;
 }
+
