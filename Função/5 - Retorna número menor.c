60b6566d4ba9b68cbc8ee79f6041c45b51547f81
Rafaela Cristina Alcântara da Silva <124393270+rafaela13c@users.noreply.github.com>
2023-10-17 09:33:02 -0300

    Add files via upload

---
+++ Função/5 - Retorna número menor.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+void res(int num1, int num2);
+
+
+int main()
+{
+    int num1, num2;
+    printf("Digite dois valores: ");
+    scanf("%d%d", &num1, &num2);
+    res(num1, num2);
+
+    return 0;
+}
+
+void res(int num1, int num2) {
+    if (num1 < num2) {
+        printf("O primeiro número é menor: %d\n", num1);
+    } else {
+        printf("O segundo número é menor: %d\n", num2);
+    }
+}
